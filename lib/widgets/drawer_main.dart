ab7ca90a9cb77e9b54d52f4b85ba4d04f9504aff
Sanskar Shrestha <sanskar.shrestha@comedi.com.hk>
2024-02-13 12:38:26 +0800

    Home button bug fixed

--- lib/widgets/drawer_main.dart
+++ lib/widgets/drawer_main.dart
@@ -1,6 +1,6 @@
-import 'package:food_info_app/main.dart';
 import 'package:food_info_app/providers/barcode_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:food_info_app/screens/main_screen.dart';
 import 'package:food_info_app/widgets/my_list_tile.dart';
 import 'package:provider/provider.dart';
 
@@ -16,7 +16,7 @@ class DrawerMain extends StatelessWidget {
       child: SafeArea(child: Consumer<BarcodeProvider>(
         builder: (context, barcode, child) {
           return ListView(children: [
-            DrawerHeader(
+            const DrawerHeader(
                 child: Icon(
               Icons.person,
               color: Colors.indigo,
@@ -26,7 +26,15 @@ class DrawerMain extends StatelessWidget {
             MyListTile(
               icon: Icons.home,
               text: 'Home',
-              onTap: () => Navigator.pop(context),
+              onTap: () {
+                Navigator.pop(context); // Close the drawer
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => MainScreen(text: ""),
+                  ),
+                );
+              },
             ),
 
             MyListTile(
